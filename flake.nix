b5ce6d9dfed7fa497fd8fd97b30932276c571442
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-03-30 18:41:32 -0500

    get nix'ed

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    # import-tree.url = "github:vic/import-tree";
   };
 
   outputs = inputs:
@@ -15,20 +14,48 @@
         "x86_64-linux"
       ];
 
-      # imports = [
-      #   (inputs.import-tree ./modules)
-      # ];
-
-      # Terranix Main Entrypoint
       perSystem = {
         pkgs,
-        inputs',
         self',
         ...
       }: let
-        inherit (inputs) self lib;
+        commonArgs = {
+          version = "devel";
+          src = ./.;
+          vendorHash = "sha256-2jNlLq+G8UI6yVb+8Fx5pN8cHT0zsAaz90lrOywDMmk=";
+          ldflags = [
+            "-extldflags '-static -s -w'"
+            "-X 'github.com/metal-stack/v.Version=devel'"
+          ];
+          tags = ["netgo" "osusergo" "urfave_cli_no_docs"];
+        };
       in {
-        packages.default = {};
+        packages = {
+          server = pkgs.buildGoModule (commonArgs
+            // {
+              pname = "server";
+              subPackages = ["cmd/server"];
+              postInstall = ''
+                mv $out/bin/server $out/bin/go-ipam-server
+              '';
+            });
+          client = pkgs.buildGoModule (commonArgs
+            // {
+              pname = "client";
+              subPackages = ["cmd/client"];
+              postInstall = ''
+                mv $out/bin/client $out/bin/go-ipam-client
+              '';
+            });
+          default = self'.packages.server;
+        };
+      };
+
+      flake.nixosModules.default = {self', ...}: {
+        environment.systemPackages = [
+          self'.packages.server
+          self'.packages.client
+        ];
       };
     };
 }
